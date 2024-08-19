b05f2c00885fe4d9e656c1e784e71723b82c3265
John Jin <johnjin56@gmail.com>
2024-08-19 18:53:22 +0900

    commit monday

---
+++ echo/echoclient.c
@@ -0,0 +1,28 @@
+#include "csapp.h"
+// argc = argument count, argv = argument vector
+int main(int argc, char **argv)
+{
+    int clientfd; // 열린 소켓 식별자를 저장할 변수
+    char *host, *port, buf[MAXLINE];
+    rio_t rio;
+
+    if (argc != 3)
+    {
+        fprintf(stderr, "usage: %s <host> <port>\n", argv[0]);
+        exit(0);
+    }
+    host = argv[1];
+    port = argv[2];
+
+    clientfd = Open_clientfd(host, port);
+    Rio_readinitb(&rio, clientfd);
+
+    while (Fgets(buf, MAXLINE, stdin) != NULL)
+    {
+        Rio_writen(clientfd, buf, strlen(buf));
+        Rio_readlineb(&rio, buf, MAXLINE);
+        Fputs(buf, stdout);
+    }
+    Close(clientfd);
+    exit(0);
+}
